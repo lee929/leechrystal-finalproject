af441e210201fd33f73ce0c184fd96124ccfbbee
lee929 <ko0kiemonster29@172-16-26-146.DYNAPOOL.NYU.EDU>
2015-12-22 08:22:32 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$(".fancybox")
+	    .fancybox({
+	        openEffect  : 'none',
+	        closeEffect : 'none',
+	        padding     : 0,
+	        margin      : [20, 60, 20, 60] // Increase left/right margin
+	    });
+	});
+	    
